833011befb069dada8536729302212ecf3292bc0
Brian Lin <brianlin16@hotmail.com>
2017-12-05 18:56:09 -0500

    Create BigSib.java

---
+++ HW4/BigSib.java
@@ -0,0 +1,19 @@
+/*
+Brian Lin 
+APCS1 pd8
+HW#4 -- A Classy Division of Labor
+*/
+ 
+public class BigSib {
+    public static void main (String[] args) {
+	BigSib.greet("Flim");
+	BigSib.greet("Flam");
+	BigSib.greet("Kazaam");
+    }
+    	public static void greet( String name ) {
+	    System.out.print("Why hello there, ");
+	    System.out.print(name);
+	    System.out.println(" How do you do?");
+	    
+    }
+}
