a350c31a4df69a4086321446fbc6985994bf7b58
Brian Lin <brianlin16@hotmail.com>
2017-12-05 18:56:58 -0500

    v2 of BigSib.java

---
+++ HW5/BigSib.java
@@ -0,0 +1,14 @@
+/*
+Brian Lin 
+APCS1 pd8 
+HW#5 -- Leave a Lasting Impression
+2017-09-24
+*/
+
+public class BigSib {
+    public static String greet( String name ) {
+	return ("Why hello there, " + name + ". How do you do?");
+    }
+    public static void main (String[] args) {
+    }
+}
