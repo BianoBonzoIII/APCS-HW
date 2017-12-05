bd15b8c5cf40e0ee9d59eae58c4c16fcf61d752c
Brian Lin <brianlin16@hotmail.com>
2017-12-05 18:52:04 -0500

    Create Greet.java

---
+++ HW1/Greet.java
@@ -0,0 +1,22 @@
+/*
+Brian Lin 
+APCS1 pd8 
+HW#1 -- Splash
+2017-09-13
+*/
+
+public class Greet {
+    public static void main (String[] args) {
+	System.out.println ("No Hablo Queso");
+    }
+}
+
+/*
+Comparisons:
+-Contains semicolon and curly brackets 
+-Function is called a class
+-public is new in Java. It doesn't appear in other languages 
+-The line of code in the middle with "static void main..." seems to 
+be some sort of "body paragraph" where the meat of the code is supposed to be
+-Print statement is a lot more complicated compared to Python and Scheme
+*/
